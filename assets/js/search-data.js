03c3cca8f96e8c46e880977dc46fb45cc58b3e8d
FeiSun <ofey.sunfei@gmail.com>
2025-01-07 13:50:31 +0000

    Deploying to gh-pages from @ STAR-SF/star-sf.github.io@3bcdf92a6502821345ed6354e425e75a160ac66f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -83,12 +83,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "people-example1",
-          title: 'Example1',
-          description: "",
-          section: "People",handler: () => {
-              window.location.href = "/people/example1/";
-            },},{id: "people-example2",
+          section: "News",},{id: "people-example2",
           title: 'Example2',
           description: "",
           section: "People",handler: () => {
