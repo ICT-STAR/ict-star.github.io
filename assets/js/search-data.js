1443f0960baf43f6112b36b9e164d8852ce3ecc3
FeiSun <ofey.sunfei@gmail.com>
2025-01-07 14:07:22 +0000

    Deploying to gh-pages from @ STAR-SF/star-sf.github.io@dfa476a222bcd99bac9057aec11cf2e8316f1f51 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,16 +72,14 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+            },{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-our-paper-unlink-to-unlearn-simplifying-edge-unlearning-in-gnns-is-accepted-by-webconf2024-congrats-to-jiajun",
+          title: 'Our paper Unlink to Unlearn: Simplifying Edge Unlearning in GNNs is accepted by...',
+          description: "",
+          section: "News",},{id: "news-three-papers-are-accepted-by-acl2024-about-model-editing-bias-in-knowledge-conflict-and-confidence-alignment-congrats-to-hexiang-wanli-and-shuchang",
+          title: 'Three papers are accepted by ACL2024 about model editing, bias in knowledge conflict,...',
           description: "",
           section: "News",},{id: "people-example2",
           title: 'Example2',
