40fd64b15ce4bcc49e76a8a0366c4f00e6264aca
gosgjkaj <gosgjkaj@outlook.com>
2025-01-08 09:01:43 +0000

    Deploying to gh-pages from @ STAR-SF/star-sf.github.io@7b27214adf809c127813b625bc9175cec3446c2a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -84,7 +84,12 @@ ninja.data = [{
           section: "News",},{id: "news-we-will-hold-the-1st-workshop-on-human-centered-recommender-systems-on-www-25-contributions-are-welcome",
           title: 'We will hold The 1st Workshop on Human-Centered Recommender Systems on WWW 25....',
           description: "",
-          section: "News",},{id: "people-jiajun",
+          section: "News",},{id: "people-haowenli",
+          title: 'Haowenli',
+          description: "",
+          section: "People",handler: () => {
+              window.location.href = "/people/haowenli.html";
+            },},{id: "people-jiajun",
           title: 'Jiajun',
           description: "",
           section: "People",handler: () => {
